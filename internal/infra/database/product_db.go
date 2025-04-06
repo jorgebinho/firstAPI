edc6ef836a5d99c9791d48fcf2da78718c575b75
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2025-04-06 12:01:00 -0300

    ✅ Add test find all products

--- internal/infra/database/product_db.go
+++ internal/infra/database/product_db.go
@@ -27,13 +27,12 @@ func (p *Product) FindAll(page, limit int, sort string) ([]entity.Product, error
 	var products []entity.Product
 	var err error
 
-	if sort != ""&& sort != "asc" && sort != "desc" {
+	if sort != "" && sort != "asc" && sort != "desc" {
 		sort = "asc"
 	}
 	if page != 0 && limit != 0 {
-		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("created_at" + sort).Find(&products).Error
+		err = p.DB.Limit(limit).Offset((page - 1) * limit).Order("created_at " + sort).Find(&products).Error
 	} else {
-		err = p.DB.Order("created_at" + sort).Find(&products).Error
 	}
 
 	return products, err
