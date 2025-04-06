b51c9720ecc5a1f88373a4439e8addf25ddc5225
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2025-04-06 10:56:09 -0300

    ✨ Add functions Create abd FindByID

---
+++ internal/infra/database/product_db.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"github.com/jorgebinho/firstApi/internal/entity"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	DB *gorm.DB
+}
+
+func NewProduct(db *gorm.DB) *Product {
+	return &Product{DB: db}
+}
+
+func (p *Product) Create(product *entity.Product) error {
+	return p.DB.Create(product).Error
+}
+
+func (p *Product) FindByID(id string) (*entity.Product, error) {
+	var product entity.Product
+	err := p.DB.First(&product, "id = ?", id).Error
+	return &product, err
+}
