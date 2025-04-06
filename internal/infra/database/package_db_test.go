148a638645c356ecfe38094f18c40150880a2977
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2025-04-06 11:08:55 -0300

    ✅ Add test createNewProduct

---
+++ internal/infra/database/package_db_test.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"testing"
+
+	"github.com/jorgebinho/firstApi/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestCreateNewProduct(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:"), &gorm.Config{})
+	if err != nil {
+		t.Error(err)
+	}
+	db.AutoMigrate(&entity.Product{})
+	product, err := entity.NewProduct("Product 1", 100)
+	assert.NoError(t, err)
+	productDB := NewProduct(db)
+	err = productDB.Create(product)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, product.ID)
+
+}
