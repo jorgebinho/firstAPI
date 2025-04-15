305d82d83bb6e1a4285161e0d10573246a8fe314
Jorge Almeida <jorgebrunoalmeidadossantos@gmail.com>
2025-04-15 18:56:34 -0300

    ✨ Add handler to create product

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/jorgebinho/firstApi/configs"
+	"github.com/jorgebinho/firstApi/internal/dto"
 	"github.com/jorgebinho/firstApi/internal/entity"
+	"github.com/jorgebinho/firstApi/internal/infra/database"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -23,4 +26,32 @@ func main() {
 	http.ListenAndServe(":8000", nil)
 }
 
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
 
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+}
